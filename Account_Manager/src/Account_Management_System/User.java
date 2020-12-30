5e6b36125db1325aebc1703918ae6a0bf750a60d
ahmedfahad04 <60494055+ahmedfahad04@users.noreply.github.com>
2020-12-31 00:33:23 +0600

    modified user input using constructor

--- Account_Manager/src/Account_Management_System/User.java
+++ Account_Manager/src/Account_Management_System/User.java
@@ -1,40 +1,29 @@
 package Account_Management_System;
 
-import java.util.Scanner;
 
 public class User {
     public void show_account_details(){
-        Scanner sc = new Scanner(System.in);
+        Account_info asif = new Account_info("Asif Ahmed Chowdhury", "01766680017");
+        Account_info safiyah = new Account_info("Safiyah Sultana", "01766610085");
 
-        Account_info first = new Account_info();
 
-        System.out.println("Enter Account details to open your account: ");
-        System.out.println("Enter your name: ");
-        String user_name = sc.nextLine();
-        System.out.println("Enter your mobile number: ");
-        String user_number = sc.nextLine();
-        System.out.println("Enter Initial Deposit amount: ");
-        int cash_in = sc.nextInt();
-        System.out.println("Account Created Successfully!!\n");
+        asif.getAccount_holder_name();
+        asif.getAccount_holder_mobile_number();
 
-        System.out.println("Enter Cash out Amount: ");
-        int cash_out = sc.nextInt();
+        asif.add_money(4000);
+        asif.cash_out(1500);
 
-        first.add_money(cash_in);
-        first.cash_out(cash_out);
-        first.setname(user_name);
-        first.setnumber(user_number);
+        asif.my_balance();
 
-        System.out.println("\n##################### Account Details #####################\n");
-        System.out.println("Name: " + first.getAccount_holder_name());
-        System.out.println("Mobile no. " + first.getAccount_holder_mobile_number());
-        System.out.println("Added Money: " + cash_in);
-        System.out.println("Requested Cashed out amount: " + cash_out);
+        System.out.println("-----------------------------------------------\n");
+        safiyah.getAccount_holder_name();
+        safiyah.getAccount_holder_mobile_number();
 
-        if(cash_in>=cash_out) {
-            System.out.println("Remaining Balance: " + first.my_balance());
-        }
-        else System.out.println("Cash Out Failed!! Not enough balance for cashing out");
+        safiyah.add_money(2500);
+        safiyah.cash_out(3000);
+
+        safiyah.my_balance();
+
 
     }
 }
