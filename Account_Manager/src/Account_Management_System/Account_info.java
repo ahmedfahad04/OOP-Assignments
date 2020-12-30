d84d4ce55c1990a50c28ce6eb9f1e337377bdcbd
ahmedfahad04 <60494055+ahmedfahad04@users.noreply.github.com>
2020-12-31 00:32:27 +0600

    constructor

--- Account_Manager/src/Account_Management_System/Account_info.java
+++ Account_Manager/src/Account_Management_System/Account_info.java
@@ -5,38 +5,42 @@ public class Account_info {
     private String account_holder_mobile_number;
     private int balance;
 
+    // constructor
+    public Account_info(String user_name, String user_phone){
+        this.account_holder_name = user_name;
+        this.account_holder_mobile_number = user_phone;
+    }
 
+    // cash out
     void cash_out(int amount){
         if(amount > balance){
-            return;
+            System.out.println("Cash out Failed due to insufficient Balance.");
         }
         else {
             this.balance -= amount;
+            System.out.println("Cash Out: " + amount);
         }
     }
 
+    // add money
     void add_money(int amount2){
         this.balance += amount2;
+        System.out.println("Cash in: " + amount2);
     }
 
-    int my_balance(){
-        return this.balance;
+    // show balance
+    void my_balance(){
+        System.out.println("Current Balance: " + this.balance);
     }
 
-    void setname(String name){
-        this.account_holder_name = name;
+    // show user name
+    public void getAccount_holder_name(){
+        System.out.println("Name: " + this.account_holder_name);
     }
 
-    void setnumber(String num){
-        this.account_holder_mobile_number = num;
-    }
-
-    public String getAccount_holder_name(){
-        return account_holder_name;
-    }
-
-    public String getAccount_holder_mobile_number(){
-        return account_holder_mobile_number;
+    // show user phone number
+    public void getAccount_holder_mobile_number(){
+        System.out.println("Cell: " + this.account_holder_mobile_number);
     }
 
 }
