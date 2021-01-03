2d0c339850eb0fb0d92f63d802e70754ad7f8e1c
ahmedfahad04 <60494055+ahmedfahad04@users.noreply.github.com>
2021-01-03 18:45:30 +0600

    Assignment(3/1/2021)
    Assignment(3/1/2021) on mobile banking management system.

---
+++ Mobile_Banking_System (Phase 1)/Main.java
@@ -0,0 +1,17 @@
+/*
+
+    author: Fahad
+    date: 3/1/2021
+
+ */
+package Account_Management_System;
+
+public class Main {
+    public static void main(String [] args){
+
+        User show_user = new User();
+        show_user.show_account_details();
+
+    }
+}
+
