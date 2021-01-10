72fe46bc478032ca95d88f8aaa7197ec97fbd907
ahmedfahad04 <60494055+ahmedfahad04@users.noreply.github.com>
2021-01-10 14:49:30 +0600

    formated output style

--- Mobile_Banking/src/Mobile_Banking_System_Phase_2/Account.java
+++ Mobile_Banking/src/Mobile_Banking_System_Phase_2/Account.java
@@ -22,38 +22,39 @@ public class Account {
     //==> Different operations(cash out, add money, send money, update pin, to string)
     // cash out
     void withdraw(double amount, String pin_code) {
-
+        System.out.println("\nCASH OUT: ");
         if (this.pin.equals(pin_code)) {
 
             if (amount > balance) {
-                System.out.println("\nCash out Failed due to insufficient Balance.");
+                System.out.println("  Cash out Failed due to insufficient Balance.");
             }
 
             else {
 
                 double withdraw_amount = (amount + (cash_out_charge * (amount/1000)));
                 this.balance -= withdraw_amount;
-                System.out.println("\nCash out: " + amount);
-                System.out.println(withdraw_amount + "/-" + " taka (including vat) cashed out successfully.");
+                System.out.println("  Cash out: " + amount);
+                System.out.println("  "+withdraw_amount + "/-" + " taka (including vat) cashed out successfully.");
                 System.out.println("Current Balance: " + balance);
             }
 
         }
 
         else {
-            System.out.println("\nWrong Pin. Money withdrawal failed!!!");
+            System.out.println("  Wrong Pin. Money withdrawal failed!!!");
         }
 
     }
 
     // add money
     void add_money(double amount2) {
+        System.out.println("\nADD MONEY: ");
         if(amount2 > 30000){
-            System.out.println("\nCash IN limitation crossed. Failed to Add Money!!!");
+            System.out.println("  Cash IN limitation crossed. Failed to Add Money!!!");
         }
         else{
             this.balance += amount2;
-            System.out.println("\nSuccessfully Added " + amount2 + "/- tk in your account.");
+            System.out.println("  Successfully Added " + amount2 + "/- tk in your account.");
             System.out.println("Current Balance: " + balance);
         }
 
@@ -61,10 +62,11 @@ public class Account {
 
     // send money
     void send_money(double amount3, String reciever, String pin_code) {
+        System.out.println("\nSEND MONEY: ");
         if (reciever != null) {
 
             if(amount3 > 25000 || amount3 > balance){
-                System.out.println("Send Money limitation exceeded. Failed to Send Money!!!");
+                System.out.println("  Send Money limitation exceeded. Failed to Send Money!!!");
             }
 
             else{
@@ -73,20 +75,20 @@ public class Account {
 
                     if (amount3 > 500) {
                         this.balance -= (amount3 + 5);
-                        System.out.println("\nSend Money: " + amount3);
-                        System.out.println((amount3 + 5) + "/-" + " taka (including vat) sent successfully.");
+                        System.out.println("  Send Money: " + amount3);
+                        System.out.println("  "+(amount3 + 5) + "/-" + " taka (including vat) sent successfully.");
                     }
 
                     else {
                         this.balance -= amount3;
-                        System.out.println(amount3 + "/-" + " taka sent successfully.");
+                        System.out.println("  "+amount3 + "/-" + " taka sent successfully.");
 
                     }
 
                 }
 
                 else {
-                    System.out.println("\nWrong Pin. Transaction Failed.");
+                    System.out.println("  Wrong Pin. Transaction Failed.");
                 }
 
             }
@@ -94,7 +96,7 @@ public class Account {
         }
 
         else {
-            System.out.println("\nReceiver number was empty. Transaction Failed!!!");
+            System.out.println("  Receiver number was empty. Transaction Failed!!!");
         }
     }
 
@@ -104,12 +106,12 @@ public class Account {
             this.pin = new_pin;
             System.out.println("\nPin updated successfully!!");
         }
-        else System.out.println("Pin update failed. Old pin doesn't matched.");
+        else System.out.println("\nPin update failed. Old pin doesn't matched.");
     }
 
     // setting new pin
     void set_new_pin(String phone_num, String new_pin){
-        System.out.println("To set new pin first you need confirm your mobile number.");
+        System.out.println("\nTo set new pin first you need confirm your mobile number.");
 
         if(phone_num.equals(this.mobile_number)) {
 
@@ -127,10 +129,10 @@ public class Account {
     public String toString() {
         return "\nAccount Details, \n" +
                 "--------------------------------------------------------------\n" +
-                "\tName: " + account_holder_name + '\n' +
-                "\tMobile Number: " + mobile_number + '\n' +
-                "\tBalance: " + balance + "/-" +'\n' +
-                "--------------------------------------------------------------\n";
+                "\tAccount Holder Name:- " + account_holder_name + '\n' +
+                "\tMobile Number:- " + mobile_number + '\n' +
+                "\tBalance:- " + balance + "/-" +'\n';
+
     }
 
     //==> function to edit Cash_out_charge
